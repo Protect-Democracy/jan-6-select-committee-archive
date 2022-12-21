332c223e47bf490965c96dbcd0b4f85f622d41b2
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2022-12-20 23:45:16 -0500

    A start.

---
+++ index.js
@@ -0,0 +1,15 @@
+// Dependencies
+import siteArchive from "./lib/site-archive.js";
+import documents from "./lib/documents.js";
+
+// siteArchive(
+//   "https://judiciary.house.gov/the-impeachment-of-donald-john-trump/",
+//   "./output",
+//   "2020-01-01"
+// );
+
+documents(
+  "https://judiciary.house.gov/the-impeachment-of-donald-john-trump/",
+  "./output",
+  "2020-01-01"
+);
